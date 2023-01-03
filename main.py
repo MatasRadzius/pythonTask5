d26cc7012e80cc2ab0c32c0efa9d4e76c6fbfa3e
Mata <matxxas@gmail.com>
2023-01-03 16:13:22 +0200

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+# Importuokite reikiamus modulius, kad veiktų žemiau nurodytos funkcijos
+
+
+# Kitų failų ir žemiau esančio kodo nekeiskite
+
+
+from modules.math.composition import composition
+from modules.math.division import division
+from modules.math.subtraction import substraction
+from modules.math.multiplication import multiplication
+from modules.numbers.numbers import one, two, four, three, five
+
+
+a = composition(one, four)
+b = division(four, two)
+c = substraction(three, two)
+d = multiplication(five, two)
+
+print(a)
+print(b)
+print(c)
+print(d)
